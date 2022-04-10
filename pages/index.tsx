6266c1d447b2160cd878f602dd3a1cab36a7e248
shema <shemamugisha>
2022-04-10 16:56:59 +0200

    creating navigation bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { NavBar } from '../components';
+import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => (
   <div className={styles.container}>
@@ -10,8 +11,39 @@ const Home: NextPage = () => (
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
+    <aside>
+      <NavBar />
+    </aside>
     <main>
-      <h1>Hello world</h1>
+      <section className={styles.heroSection}>
+        <div>
+          <h1>Hero section</h1>
+        </div>
+      </section>
+
+      <section className={styles.AboutSection}>
+        <div>
+          <h1>About section</h1>
+        </div>
+      </section>
+
+      <section className={styles.SkillsSection}>
+        <div>
+          <h1>Skills section</h1>
+        </div>
+      </section>
+
+      <section className={styles.WorkSection}>
+        <div>
+          <h1>Work section</h1>
+        </div>
+      </section>
+
+      <section className={styles.ContactSection}>
+        <div>
+          <h1>Contact section</h1>
+        </div>
+      </section>
     </main>
   </div>
 );
