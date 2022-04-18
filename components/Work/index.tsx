06f026de62405d0f7b4ae8d263b60ceed4f7d353
shema <shemamugisha>
2022-04-19 01:11:20 +0200

    making responsive remaining sections

---
+++ components/Work/index.tsx
@@ -0,0 +1,175 @@
+/* eslint-disable react/jsx-wrap-multilines */
+/* eslint-disable react/no-children-prop */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+import { FC, ReactNode, useState } from 'react';
+import Link from 'next/link';
+import { BiWindowOpen } from 'react-icons/bi';
+import { VscSourceControl } from 'react-icons/vsc';
+import { AiFillGithub } from 'react-icons/ai';
+
+import styles from './styles.module.scss';
+
+type Tcard = {
+  title: string;
+  footer: ReactNode;
+  content: string;
+  children: ReactNode;
+};
+
+type Tlink = {
+  link: string;
+};
+
+const Link1: FC<Tlink> = ({ link }) => (
+  <Link href={link}>
+    <BiWindowOpen className={styles.icon1} />
+  </Link>
+);
+
+const Link2: FC<Tlink> = ({ link }) => (
+  <Link href={link}>
+    <VscSourceControl className={styles.icon2} />
+  </Link>
+);
+
+const Card: FC<Tcard> = ({ title, content, footer, children }) => (
+  <div className={styles.card}>
+    <header>
+      <h1>{title}</h1>
+      <div>{children}</div>
+    </header>
+
+    <main>
+      <p>{content}</p>
+    </main>
+    <footer>{footer}</footer>
+  </div>
+);
+
+const Work: FC = () => {
+  const [work, useWork] = useState('');
+  return (
+    <div className={styles.container}>
+      <div className={styles.cardContainer}>
+        <Card
+          children={<Link1 link="hah" />}
+          title="Fiesta"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. A magnam doloremque aperiam unde, corrupti nisi non
+        adipisci harum fugiat voluptas?"
+          footer={
+            <>
+              <p>Aws</p>
+              <p>NextJs</p>
+              <p>Bootsrap</p>
+              <p>Graphql</p>
+            </>
+          }
+        />
+
+        <Card
+          children={<Link1 link="hah" />}
+          title="COA"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. A magnam doloremque aperiam unde, corrupti nisi non
+        adipisci harum fugiat voluptas?"
+          footer={
+            <>
+              <p>Aws</p>
+              <p>NextJs</p>
+              <p>Bootsrap</p>
+              <p>Graphql</p>
+            </>
+          }
+        />
+
+        <Card
+          children={
+            <>
+              <Link1 link="hah" />
+              <Link2 link="haha" />
+            </>
+          }
+          title="Fiesta"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. A magnam doloremque aperiam unde, corrupti nisi non
+        adipisci harum fugiat voluptas?"
+          footer={
+            <>
+              <p>Aws</p>
+              <p>NextJs</p>
+              <p>Bootsrap</p>
+              <p>Graphql</p>
+            </>
+          }
+        />
+
+        <Card
+          children={
+            <>
+              <Link1 link="hah" />
+              <Link2 link="haha" />
+            </>
+          }
+          title="Fiesta"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. A magnam doloremque aperiam unde, corrupti nisi non
+        adipisci harum fugiat voluptas?"
+          footer={
+            <>
+              <p>Aws</p>
+              <p>NextJs</p>
+              <p>Bootsrap</p>
+              <p>Graphql</p>
+            </>
+          }
+        />
+
+        <Card
+          children={
+            <>
+              <Link1 link="hah" />
+              <Link2 link="haha" />
+            </>
+          }
+          title="Fiesta"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. A magnam doloremque aperiam unde, corrupti nisi non
+        adipisci harum fugiat voluptas?"
+          footer={
+            <>
+              <p>Aws</p>
+              <p>NextJs</p>
+              <p>Bootsrap</p>
+              <p>Graphql</p>
+            </>
+          }
+        />
+
+        <Card
+          children={
+            <>
+              <Link1 link="hah" />
+              <Link2 link="haha" />
+            </>
+          }
+          title="Fiesta"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. A magnam doloremque aperiam unde, corrupti nisi non
+        adipisci harum fugiat voluptas?"
+          footer={
+            <>
+              <p>Aws</p>
+              <p>NextJs</p>
+              <p>Bootsrap</p>
+              <p>Graphql</p>
+            </>
+          }
+        />
+      </div>
+
+      <div className={`${styles.moreBtn}`}>
+        <a href="https://github.com/shemamugisha" className="appBtn">
+          More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { NavBar, Skills } from '../components';
+import { NavBar, Skills, Work, Contact, About, Hero } from '../components';
 import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => (
@@ -15,34 +15,33 @@ const Home: NextPage = () => (
       <NavBar />
     </aside>
     <main>
-      <section>
-        <div>
-          <h1>Hero section</h1>
-        </div>
+      <section className={styles.hero}>
+        <Hero />
       </section>
 
-      <section>
-        <div>
-          <h1>About section</h1>
-        </div>
+      <section className={styles.about}>
+        <h1 className={styles.header}>About Me</h1>
+        <About />
       </section>
 
-      <section className="" style={{}}>
-        <h1>Key Skills</h1>
+      <section className={styles.skills}>
+        <h1 className={styles.header}> Key Skills</h1>
         <Skills />
       </section>
 
-      <section>
-        <div>
-          <h1>Work section</h1>
-        </div>
+      <section className={styles.work}>
+        <h1 className={styles.header}>Recent Work</h1>
+        <Work />
       </section>
 
-      <section>
-        <div>
-          <h1>Contact section</h1>
-        </div>
+      <section className={styles.contact}>
+        <h1 className={styles.header}>Contact Me</h1>
+        <Contact />
       </section>
+
+      <footer className={styles.footer}>
+        <p>Shema © 2022 All Rights Reserved</p>
+      </footer>
     </main>
   </div>
 );
